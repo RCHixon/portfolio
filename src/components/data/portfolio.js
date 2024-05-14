dbccd631a252a8ab95e5fae18161a944d79b2a1a
chixon <christopher.hixon@me.com>
2024-05-14 17:54:14 -0400

    Initial Commit

---
+++ src/components/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+   {
+      title: 'MFD Dashboard App',
+      imgUrl: '/assets/terminal-website.jpg',
+      stack: ['Django', 'JavaScript', 'Python', 'HTML', 'CSS'],
+      link: 'temp',
+   },
+   {
+      title: 'Developer Portfolio',
+      imgUrl: '/assets/dev-portfolio.jpg',
+      stack: ['React', 'Tailwind', 'Vite'],
+      link: 'temp',
+   },
+   {
+      title: 'Job Tracker App',
+      imgUrl: '/assets/terminal-website.jpg',
+      stack: ['Django', 'JavaScript', 'Python', 'HTML', 'CSS'],
+      link: 'temp',
+   },
+   {
+      title: 'Device Erasure App',
+      imgUrl: '/assets/terminal-website.jpg',
+      stack: ['Django', 'JavaScript', 'Python', 'HTML', 'CSS'],
+      link: 'temp',
+   },
+]
---
+++ src/components/data/timeline.js
@@ -0,0 +1,14 @@
+export default [
+   {
+      year: 'Today',
+      title: 'Full Stack Engineer',
+      duration: '5 years',
+      details: 'I build web apps from the ground up for internal operations for data center and desktop support teams.'
+   },
+   {
+      year: '2019',
+      title: "Bachelor's Degree in Information Technology",
+      duration: '5 years',
+      details: 'Pursued the knowledge that an information technology degree at Georgia Southern University had to offer. I learned discrete mathematics, object-oriented programming, data programming, machine learning, datamining algorithms and techniques, IT management, and information management.'
+   },
+]
