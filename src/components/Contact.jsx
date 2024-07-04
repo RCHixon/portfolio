697f3c06f191bae24adf958b42888dfd6b50c0a0
Chris Hixon <christopher.hixon@me.com>
2024-07-04 16:40:56 -0400

    updated getform tag

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <div className="flex flex-col mb-10 mx-auto">
          <div className="flex justify-center items-center">
             <form
-               action="https://getform.io/f/lajkxqnb"
+               action="https://getform.io/f/axojjopb"
                method="POST"
                className="flex flex-col w-full md:w-7/12"
             >
